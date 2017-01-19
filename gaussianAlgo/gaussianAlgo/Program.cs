3f3850067a1fc27f17a329f5b3792ae3ad8d7733
Saad Abdullah Gondal <sag_21jan@hotmail.com>
2017-01-19 20:42:30 +0100

    Formatting performed

--- gaussianAlgo/gaussianAlgo/Program.cs
+++ gaussianAlgo/gaussianAlgo/Program.cs
@@ -1,32 +1,19 @@
-﻿
-using System;
-
+﻿using System;
 using System.Collections.Generic;
-
 using System.Linq;
-
 using System.Text;
-
 using System.Threading.Tasks;
 
-
-
-
 namespace GaussiaonEliminationSequential
-
 {
 	class Program
 	{
-
 		static void Main(string[] args)
 		{
-
 			double[,] A = new double[,]
 			{
 				{5,6,8,5},
-
 				{3,-6,1,7},
-
 				{4,9,7,10}
 			};
 
@@ -35,7 +22,6 @@ namespace GaussiaonEliminationSequential
 			int N = 3;
 			double multiplicationFactor;
 			double divisionFactor;
-
 
 			for (int diagonal = 0; diagonal < N - 1; diagonal++)
 			{
@@ -46,66 +32,37 @@ namespace GaussiaonEliminationSequential
 				divisionFactor = A[diagonal, diagonal];
 
 				for (int i = diagonal + 1; i < N; i++)
-
 				{
 					multiplicationFactor = A[i, diagonal];
 
 					if (multiplicationFactor == 0)
 						continue;
 
-					if ( multiplicationFactor > 0)
+					if (multiplicationFactor > 0)
 
 						multiplicationFactor *= -1;
 
 					else
-
 						multiplicationFactor = Math.Abs(multiplicationFactor);
-
 
 					Console.WriteLine("Multiplication Factor : {0}", multiplicationFactor);
 
-
 					for (int j = diagonal; j < N + 1; j++)
 					{
-						if (i == diagonal + 1) 
+						if (i == diagonal + 1)
 							A[diagonal, j] = A[diagonal, j] / divisionFactor;
-						
-						A[i, j] = A[i, j] + (A[diagonal, j] * multiplicationFactor);
 
+						A[i, j] = A[i, j] + (A[diagonal, j] * multiplicationFactor);
 					}
 
-
 					for (int ii = 0; ii < N; Console.WriteLine(), ii++)
-
 						for (int j = 0; j < N + 1; Console.Write("{0}+++", A[ii, j]), j++) ;
-
 				}
-
-
-
-
-				//for (int i = 0; i < N; Console.WriteLine(), i++)
-
-				//    for (int j = 0; j < N; Console.Write("{0}+++", A[i, j]), j++) ;
-
 			}
 
-
-
-
 			Console.WriteLine();
-
-
-
-
-			//for (int i = 0; i < N; Console.WriteLine(), i++)
-
-			//    for (int j = 0; j < N; Console.Write("{0}+++", A[i, j]), j++) ;
-
 		}
-
 	}
-
 }
 
 
